adb34986027679ec45edf2e36cb1162aa63d8e8a
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-17 01:29:45 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,11 +25,11 @@ def adding
                     }
                     
   shipping_manifest["muskets"] = 2
- puts shipping_manifest
+  shipping_manifest
 
 
   shipping_manifest["gun powder"] = 4
-   puts shipping_manifest
+   shipping_manifest
 end
 
 

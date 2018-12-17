b29e01444de6b812ea0ac2ffb7b5d06b4a7bb069
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-17 01:25:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,8 +2,6 @@ def my_hash
   my_hash = {"dog" => "Benji the Rottie", "cat" => "Puss in Boots" }
 end
 
-
-
 def shipping_manifest
   the_manifest = {"whale bone corsets" => 5,
   "porcelain vases" => 2,
